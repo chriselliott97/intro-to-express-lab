6afe25e683e651f73125a8092e87a0217626b8aa
chriselliott97 <chris.elliott6497@gmail.com>
2022-09-19 18:20:28 -0400

    add data

---
+++ data/students.js
@@ -0,0 +1,12 @@
+const students = [
+  {Name: 'Joe Schmoe', active: true, _id: 1234556},
+  {Name: 'Sarah Sota', active: false, _id: 1234556},
+  {Name: 'Peter Griffin', active: true, _id: 1234556},
+  {Name: 'Eren Yeager', active: true, _id: 1234556},
+  {Name: 'Mongo Du Bois', active: true, _id: 1234556},
+  {Name: 'Jeff', active: false, _id: 1234556}
+]
+
+export {
+  students
+}
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 //  import modules
 import express from 'express'
+import { students } from './data/students.js'
 //  create express app 
 const app = express()
+
 //  configure the app (app.set)
 app.set('view engine', 'ejs')
+
 //  mount middleware (app.use)
 
-//  mount routes 
 
+//  mount routes 
 app.get('/', function(req, res) {
   res.redirect('/students')
 })
@@ -16,6 +19,11 @@ app.get('/students', function(req, res) {
   res.render('students')
 })
 
+app.get('/students', function(req, res) {
+  res.render('students', {
+    students: students
+  })
+})
 app.listen(3000, function() {
   console.log('Listening on port 3000')
 })
