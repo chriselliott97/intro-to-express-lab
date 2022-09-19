910a39c9a99a16ea2df30713636a0b57c981f0c7
chriselliott97 <chris.elliott6497@gmail.com>
2022-09-19 18:05:31 -0400

    create browser

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ import express from 'express'
 //  create express app 
 const app = express()
 //  configure the app (app.set)
-
+app.set('view engine', 'ejs')
 //  mount middleware (app.use)
 
 //  mount routes 
 
+app.get('/', function(req, res) {
+  res.redirect('/students')
+})
 
+app.get('/students', function(req, res) {
+  res.render('students')
+})
 
 app.listen(3000, function() {
   console.log('Listening on port 3000')
